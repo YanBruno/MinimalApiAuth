9dbe57d0a0d289a5c310a13e62ae65508da2b427
yan.santos <yan.santos@midway.com.br>
2021-10-21 01:16:37 -0300

    api complete

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
+using System.Security.Claims;
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using MinimalApi.Services;
 using MinimalApiAuth;
+using MinimalApiAuth.Models;
+using MinimalApiAuth.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
 var key = Encoding.ASCII.GetBytes(Settings.SecretTokenKey);
@@ -38,5 +42,35 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", async (User model) => {
+    var user = await UserRepository.GetUser(model.Username, model.Password);
+    if(user == null)
+        return Results.NotFound(new {
+            message = "Usuário não encontrado"
+        });
+
+    var token = TokenService.GenerateToken(user);
+    user.Password = "null";
+
+    return Results.Ok(new {
+        user,
+        token
+    });
+});
+
+app.MapGet("/anonymous", ()=>{ Results.Ok("Anônimo"); }).AllowAnonymous();
+
+app.MapGet("/authenticated", (ClaimsPrincipal user)=>{
+     Results.Ok($"{user.Identity.Name}"); 
+}).RequireAuthorization();
+
+app.MapGet("/employee", (ClaimsPrincipal user)=>{
+     Results.Ok($"{user.Identity.Name}"); 
+}).RequireAuthorization("Employee");
+
+app.MapGet("/manager", (ClaimsPrincipal user)=>{
+     Results.Ok($"Manager - {user.Identity.Name}"); 
+}).RequireAuthorization("Admin");
 
 app.Run();
